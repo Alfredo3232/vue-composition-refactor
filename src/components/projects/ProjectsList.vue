340dc33d172e70b17b24937bf934df93d74ee993
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-13 18:46:24 -0600

    initial commit

---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,78 @@
+<template>
+  <base-container v-if="user">
+    <h2>{{ user.fullName }}: Projects</h2>
+    <base-search
+      v-if="hasProjects"
+      :search-term="enteredSearchTerm"
+      @search="updateSearch"
+    />
+    <ul v-if="hasProjects">
+      <project-item
+        v-for="prj in availableProjects"
+        :key="prj.id"
+        :title="prj.title"
+      />
+    </ul>
+    <h3 v-else>
+      No projects found.
+    </h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+
+<script>
+import ProjectItem from "./ProjectItem.vue";
+
+export default {
+  components: {
+    ProjectItem,
+  },
+  props: ["user"],
+  data() {
+    return {
+      enteredSearchTerm: "",
+      activeSearchTerm: "",
+    };
+  },
+  computed: {
+    hasProjects() {
+      return this.user.projects && this.availableProjects.length > 0;
+    },
+    availableProjects() {
+      if (this.activeSearchTerm) {
+        return this.user.projects.filter((prj) =>
+          prj.title.includes(this.activeSearchTerm)
+        );
+      }
+      return this.user.projects;
+    },
+  },
+  watch: {
+    enteredSearchTerm(val) {
+      setTimeout(() => {
+        if (val === this.enteredSearchTerm) {
+          this.activeSearchTerm = val;
+        }
+      }, 300);
+    },
+    user() {
+      this.enteredSearchTerm = "";
+    },
+  },
+  methods: {
+    updateSearch(val) {
+      this.enteredSearchTerm = val;
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/users/UserList.vue
@@ -0,0 +1,104 @@
+<template>
+  <base-container>
+    <h2>Active Users</h2>
+    <base-search
+      :search-term="enteredSearchTerm"
+      @search="updateSearch"
+    />
+    <div>
+      <button
+        :class="{ selected: sorting === 'asc' }"
+        @click="sort('asc')"
+      >
+        Sort Ascending
+      </button>
+      <button
+        :class="{ selected: sorting === 'desc' }"
+        @click="sort('desc')"
+      >
+        Sort Descending
+      </button>
+    </div>
+    <ul>
+      <user-item
+        v-for="user in displayedUsers"
+        :id="user.id"
+        :key="user.id"
+        :user-name="user.fullName"
+        @list-projects="$emit('list-projects', $event)"
+      />
+    </ul>
+  </base-container>
+</template>
+
+<script>
+import UserItem from "./UserItem.vue";
+
+export default {
+  components: {
+    UserItem,
+  },
+  props: ["users"],
+  data() {
+    return {
+      enteredSearchTerm: "",
+      activeSearchTerm: "",
+      sorting: null,
+    };
+  },
+  computed: {
+    availableUsers() {
+      let users = [];
+      if (this.activeSearchTerm) {
+        users = this.users.filter((usr) =>
+          usr.fullName.includes(this.activeSearchTerm)
+        );
+      } else if (this.users) {
+        users = this.users;
+      }
+      return users;
+    },
+    displayedUsers() {
+      if (!this.sorting) {
+        return this.availableUsers;
+      }
+      return this.availableUsers.slice().sort((u1, u2) => {
+        if (this.sorting === "asc" && u1.fullName > u2.fullName) {
+          return 1;
+        } else if (this.sorting === "asc") {
+          return -1;
+        } else if (this.sorting === "desc" && u1.fullName > u2.fullName) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    },
+  },
+  watch: {
+    enteredSearchTerm(val) {
+      setTimeout(() => {
+        if (val === this.enteredSearchTerm) {
+          this.activeSearchTerm = val;
+        }
+      }, 300);
+    }
+  },
+  methods: {
+    updateSearch(val) {
+      this.enteredSearchTerm = val;
+    },
+    sort(mode) {
+      this.sorting = mode;
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
