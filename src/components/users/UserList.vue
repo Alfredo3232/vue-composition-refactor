f9d3e3ee46ecc6bbd64f0acc4c58359376bba7f4
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-14 19:04:24 -0600

    refactor UserList to use setup

--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -32,6 +32,7 @@
 </template>
 
 <script>
+import { ref, computed, watch } from "vue";
 import UserItem from "./UserItem.vue";
 
 export default {
@@ -39,58 +40,70 @@ export default {
     UserItem,
   },
   props: ["users"],
-  data() {
-    return {
-      enteredSearchTerm: "",
-      activeSearchTerm: "",
-      sorting: null
-    };
-  },
-  computed: {
-    availableUsers() {
+  setup(props, context) {
+    let enteredSearchTerm = ref("");
+    let activeSearchTerm = ref("");
+    let sorting = ref(null);
+
+    // methods functions
+    function updateSearch(val) {
+      enteredSearchTerm.value = val;
+    }
+    function sort(mode) {
+      sorting.value = mode;
+    }
+
+    // computed functions
+    let availableUsers = computed(() => {
       let users = [];
-      if (this.activeSearchTerm) {
-        users = this.users.filter((usr) =>
-          usr.fullName.includes(this.activeSearchTerm)
+      if (activeSearchTerm.value) {
+        users = props.users.filter((usr) =>
+          usr.fullName.includes(activeSearchTerm.value)
         );
-      } else if (this.users) {
-        users = this.users;
+      } else if (props.users) {
+        users = props.users;
       }
       return users;
-    },
-    displayedUsers() {
-      if (!this.sorting) {
-        return this.availableUsers;
+    });
+    let displayedUsers = computed(() => {
+      if (!sorting.value) {
+        return availableUsers.value;
       }
-      return this.availableUsers.slice().sort((u1, u2) => {
-        if (this.sorting === "asc" && u1.fullName > u2.fullName) {
+      return availableUsers.value.slice().sort((u1, u2) => {
+        if (sorting.value === "asc" && u1.fullName > u2.fullName) {
           return 1;
-        } else if (this.sorting === "asc") {
+        } else if (sorting.value === "asc") {
           return -1;
-        } else if (this.sorting === "desc" && u1.fullName > u2.fullName) {
+        } else if (sorting.value === "desc" && u1.fullName > u2.fullName) {
           return -1;
         } else {
           return 1;
         }
       });
-    }
-  },
-  watch: {
-    enteredSearchTerm(val) {
+    });
+
+    // watch functions
+    watch(enteredSearchTerm, function (val) {
       setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
+        if (val === enteredSearchTerm.value) {
+          activeSearchTerm.value = val;
         }
       }, 300);
-    }
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-    sort(mode) {
-      this.sorting = mode;
-    }
+    });
+
+    // return
+    return {
+      // data
+      enteredSearchTerm,
+      activeSearchTerm,
+      sorting,
+      //methods
+      updateSearch,
+      sort,
+      // computed
+      availableUsers,
+      displayedUsers
+    };
   }
 };
 </script>
